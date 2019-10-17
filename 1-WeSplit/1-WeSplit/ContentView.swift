28717e4ec4c84985dc63aee90056850e39dbce2f
audreywelch <welch.audreyl@gmail.com>
2019-10-16 19:20:19 -0700

    first swiftUI project created

---
+++ 1-WeSplit/1-WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  1-WeSplit
+//
+//  Created by Audrey Welch on 10/16/19.
+//  Copyright © 2019 Audrey Welch. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

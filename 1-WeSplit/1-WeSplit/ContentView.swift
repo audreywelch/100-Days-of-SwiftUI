21f873b207c606c280a5e9a773475653df7e413e
audreywelch <welch.audreyl@gmail.com>
2019-11-06 11:07:24 -0800

    Modified program state using @State

--- 1-WeSplit/1-WeSplit/ContentView.swift
+++ 1-WeSplit/1-WeSplit/ContentView.swift
@@ -11,30 +11,38 @@ import SwiftUI
 // Struct that conforms to the View protocol - must be adopted by anything you want to draw on the screen
 struct ContentView: View {
     
+    // Property Wrapper: special attribute to place before our properties that allows us
+    // to change a value even though our view is a struct
+    @State private var tapCount = 0
+    
     // Define a new computed property called `body` which has the type `some View`
     // Meaning it will return something that conforms to the View protocol
     // `some` keyword adds a restriction: it must always be the _same_ kind of view being returned
     var body: some View {
         
-        NavigationView {
-            // Create a basic form
-            Form {
-                // When grouping, 10 children per parent
-                //Group {
-                Section {
-                    // Create a text view using "Hello World" string
-                    Text("Hello World")
-
-                }
-            }
-            // Attach a modifier
-            // Similar to regular methods, but always returns a new instance
-            // of whatever you use them on
-            // Default: Large Title
-            //.navigationBarTitle(Text("SwiftUI"))
-            //.navigationBarTitle("SwiftUI", displayMode: .inline)
-            .navigationBarTitle("SwiftUI")
+        Button("Tap Count: \(tapCount)") {
+            self.tapCount += 1
         }
+        
+//        NavigationView {
+//            // Create a basic form
+//            Form {
+//                // When grouping, 10 children per parent
+//                //Group {
+//                Section {
+//                    // Create a text view using "Hello World" string
+//                    Text("Hello World")
+//
+//                }
+//            }
+//            // Attach a modifier
+//            // Similar to regular methods, but always returns a new instance
+//            // of whatever you use them on
+//            // Default: Large Title
+//            //.navigationBarTitle(Text("SwiftUI"))
+//            //.navigationBarTitle("SwiftUI", displayMode: .inline)
+//            .navigationBarTitle("SwiftUI")
+//        }
     }
 }
 

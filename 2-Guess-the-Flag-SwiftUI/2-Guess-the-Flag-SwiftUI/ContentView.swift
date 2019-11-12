0bdb2d34b5b7d3a46761ebd3f074f1cceec32671
audreywelch <welch.audreyl@gmail.com>
2019-11-11 20:43:12 -0800

    Started project 2 and used stacks to arrange views

---
+++ 2-Guess-the-Flag-SwiftUI/2-Guess-the-Flag-SwiftUI/ContentView.swift
@@ -0,0 +1,55 @@
+//
+//  ContentView.swift
+//  2-Guess-the-Flag-SwiftUI
+//
+//  Created by Audrey Welch on 11/11/19.
+//  Copyright © 2019 Audrey Welch. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        // Vertical Stack (VStack)
+        // Horizontal Stack (HStack)
+        // ZStack arranges things by depth - making views that overlap
+        
+        // By default, VStack places little or no spacing between the views,
+        // so provide a parameter
+        
+        // By default, Vstack aligns its views so they are centered,
+        // but you can control it by its alignment property
+        VStack(alignment: .leading, spacing: 20) {
+
+            Text("Hello World")
+            Text("This is inside a stack")
+
+            // Stacks automatically fit their content, and prefer to align themselves
+            // to the center of the available space
+            // To change, use one or more Spacer views to push the contents
+            // of the stack to one side
+            // These automatically take up all remaining space
+
+            Spacer()
+
+            // If you add more than one spacer they will divide the available space between them
+        }
+        
+        // ZStack doesn't have spacing, only alignment - if you have one large thing
+        // and one small thing inside a ZStack, you can make both views align to the top
+        // ZStack(alignment: .top) {
+        
+        // ZStack draws its content from top to bottom, back to front
+        // If you have an image then some text, ZStack will draw them in that order,
+        // placing the text on top of the image
+        
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
